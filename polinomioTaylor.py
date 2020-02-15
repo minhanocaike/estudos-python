7a1fa712fc7860cc2df8576ddd4c1326b595ea76
caike.minhano <caike.minhano@terceiroslinx.com.br>
2020-02-15 20:34:06 -0300

    Implementação da função de calculo da raiz quadrada

--- polinomioTaylor.py
+++ polinomioTaylor.py
@@ -1,54 +1,56 @@
 ##
+#
 # @ Author Caike Minhano Campos
-# @File Algoritmo para calular o polinômio de Taylor de ordem 1 
-# #
-
-import math;
+# @File Algoritmo para calular o polinômio de Taylor de ordem 1 para funções do tipo raiz quadrada de x
+#
+##
 
 ######################################################################### 
 #                                                                       #
-#  Declaração da variaves necessárias para implementação do algoritmo   #
+#  Declaração da variáveis necessárias para implementação do algoritmo  #
 #                                                                       # 
 #########################################################################
 
-
 # Variável x que será inputada pelo usuário
-x = float(input("Digite o valor da variavel a ser aproximada:\n"));
+x = float(input("Digite o valor da variavel a ser aproximada:\n"))
 
 # Variável que irá receber o valor aproximado de x para base de aproximacação
-x0 = int(x);
+x0 = int(x)
 
 # Print do valor aproximado de X
-print("Valor para X0 = {}" .format(x0));
+print("Valor para X0 = {}".format(x0))
 
 ######################################################################### 
 #                                                                       #
 #          Funções para calcular a f(x), f'(x) e T(x) onde:             #    
 #                                                                       #
 #                         f'(x) é a derivada;                           #
-#          T(x) e função que descreve a reta tangente ao ponto X0       #
+#       T(x) é a função que descreve a reta tangente ao ponto X0        #
 #                                                                       # 
 #########################################################################
 
-# Função para calcular a raiz quadrada
-def raizQuadrada(x):
-    pass;
+# Função para calcular a raiz quadrada, melhoria para eliminar o modulo math e otimizar o codigo
+def raizQuadrada(variableX): #TODO
+    for i in range(int(x/2),0,-1):
+        if((variableX/i) == i):
+            return i
+
+# Atribui o valor da raiz de X0 à variável raiz
+raiz = raizQuadrada(x0)
 
 # Função de F(x)
-def calculaFuncaoX(variableX0):
+def calculaFuncaoX():
     #print("Raiz de X0 = {}".format(math.sqrt(variableX0)));
-    return math.sqrt(variableX0); 
+    return raiz
 
 # Função de F'(X)
-def calculaDerivadaX(variableX0):
+def calculaDerivadaX():
     #print("Deriva de X0 = {}".format((1/(2 * math.sqrt(variableX0)))));
-    return float(1/(2 * math.sqrt(variableX0)));
+    return float(1/(2 * raiz))
 
 # Função para calcular a reta Tangente de x, T(x)
 def calculaRetaTangenteX(variableX0, variableX):
-    return float(calculaFuncaoX(variableX0) + (calculaDerivadaX(variableX0) * (variableX-variableX0)));
+    return float(calculaFuncaoX() + (calculaDerivadaX() * (variableX-variableX0)))
 
 # Imprime o resultado da aproximação
-print("Valor aproximado = {}" .format(float(calculaRetaTangenteX(x0,x))));
-
-
+print("Valor aproximado = {}" .format(float(calculaRetaTangenteX(x0,x))))
