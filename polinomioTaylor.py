5e5f97cb57a9ce9808b8ee0ac4f19d9753347a6c
Caike Minhano <caike.minhano@terceiroslinx.com.br>
2020-02-17 14:09:20 -0300

    Criação da estrutura para a função de cálculo da estimativa de erro

--- polinomioTaylor.py
+++ polinomioTaylor.py
@@ -1,7 +1,8 @@
 ##
 #
 # @ Author Caike Minhano Campos
-# @File Algoritmo para calular o polinômio de Taylor de ordem 1 para funções do tipo raiz quadrada de x
+# @ File Algoritmo para calular o polinômio de Taylor de ordem 1 para funções do tipo raiz quadrada de x
+# Para valores próximos de x no intervalo -----------
 #
 ##
 
@@ -30,7 +31,7 @@ print("Valor para X0 = {}".format(x0))
 #########################################################################
 
 # Função para calcular a raiz quadrada, melhoria para eliminar o modulo math e otimizar o codigo
-def raizQuadrada(variableX): #TODO
+def raizQuadrada(variableX):
     for i in range(int(x/2),0,-1):
         if((variableX/i) == i):
             return i
@@ -48,9 +49,31 @@ def calculaDerivadaX():
     #print("Deriva de X0 = {}".format((1/(2 * math.sqrt(variableX0)))));
     return float(1/(2 * raiz))
 
+##
 # Função para calcular a reta Tangente de x, T(x)
+# Polinomio com o seguinte modelo T(x) = f(x) + f'(x)*(X-X0)
+##
 def calculaRetaTangenteX(variableX0, variableX):
     return float(calculaFuncaoX() + (calculaDerivadaX() * (variableX-variableX0)))
 
 # Imprime o resultado da aproximação
 print("Valor aproximado = {}" .format(float(calculaRetaTangenteX(x0,x))))
+
+######################################################################### 
+#                                                                       #
+#          Cálculo da estimativa de erro E(x) = f(x) - T(x)             #    
+#                                                                       #
+#                 f(x) é a função que define a curva;                   #
+#       T(x) é a função que descreve a reta tangente ao ponto X0        #
+#                                                                       # 
+#########################################################################
+
+##
+# Função para calcular o quadrado da da diferença dos dois pontos
+##
+def estimativaErro(variableX0,variableX):
+    pass
+
+
+
+
